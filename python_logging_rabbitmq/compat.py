db69f80e532e7f3341f3840a1fb9c37bef9f854b
Carlos Alvarez Novoa <carlos.alvarez@elparking.com>
2017-03-29 10:05:28 +0200

    fix compatibility to Python 3

--- python_logging_rabbitmq/compat.py
+++ python_logging_rabbitmq/compat.py
@@ -5,8 +5,10 @@ import sys
 
 if sys.version_info[0] == 2:
     text_type = unicode
+    from Queue import Queue as Queue
 else:
     text_type = str
+    from queue import Queue as Queue
 
 try:
     import ujson as json  # noqa: F401
